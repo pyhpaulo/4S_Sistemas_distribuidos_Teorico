e2c557b79100c8a5aeee28666e2cb71cbae2406a
Paulo Henrique Domingos <90566724+pyhpaulo@users.noreply.github.com>
2023-11-03 11:21:25 -0300

    Metodos GET/POST/UPDATE/DELETE
    Codigo atualizado

---
+++ Sistemas distribuidos - Prof Montanha/ProdutosApiAula/ProdutosApiAula/Controllers/ProdutoController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutosApiAula.Models;
+using ProdutosApiAula.Repositories;
+
+namespace ProdutosApiAula.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutoController : Controller
+    {
+        [HttpGet(Name ="GetProduto")]
+        public IEnumerable<Produto> Get()
+        {
+            return MockDB.GetProdutos();
+        }
+
+        [HttpGet("{id}")]
+        public Produto GetProduto(int id) 
+        {
+            return MockDB.GetProduto(id);
+        }
+
+        [HttpPost]
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Produto produto)
+        {
+            MockDB.Produtos.Add(produto);
+            return CreatedAtRoute("GetProduto", new { id = produto.Id }, produto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Produto produto)
+        {
+            var existingProduto = MockDB.GetProduto(id);
+            if (existingProduto == null)
+            {
+                return NotFound();
+            }
+
+            existingProduto.Name = produto.Name;
+            existingProduto.Description = produto.Description;
+            existingProduto.Price = produto.Price;
+            existingProduto.Quantity = produto.Quantity;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var produto = MockDB.GetProduto(id);
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            MockDB.Produtos.Remove(produto);
+
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Sistemas distribuidos - Prof Montanha/ProdutosApiAula/ProdutosApiAula/Repositories/MockDB.cs
@@ -0,0 +1,37 @@
+﻿using ProdutosApiAula.Models;
+
+namespace ProdutosApiAula.Repositories
+{
+    public static class MockDB
+    {
+        public static List<Produto> Produtos { get; set; } = new List<Produto>();
+        //Faça um método que retorne 10 produtos Mockados
+
+        public static List<Produto> GetProdutos()
+        {
+            generateProdutos();
+            return Produtos;
+        }
+
+        public static Produto? GetProduto(int id)
+        {
+            generateProdutos();
+            return Produtos.FirstOrDefault(p => p.Id == id);
+        }
+
+        private static void generateProdutos()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Produto produto = new Produto();
+                produto.Id = i;
+                produto.Name = $"Produto {i}";
+                produto.Description = $"Descrição do produto {i}";
+                produto.Price = i * 10;
+                produto.Quantity = i * 2;
+                Produtos.Add(produto);
+            }
+        }
+
+    }
+}
